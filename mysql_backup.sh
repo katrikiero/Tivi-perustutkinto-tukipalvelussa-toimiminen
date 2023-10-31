79248fb4f3c11b1a2bba517d06f71c9c130f7fb8
KatriKiero <111054270+katrikiero@users.noreply.github.com>
2023-10-31 21:32:20 +0200

    Update mysql_backup.sh

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -2,7 +2,9 @@
 
 DB_USER="root" DB_PASSWORD="Katri istuu kadulla klo 15" DB_NAME="wp_database" BACKUP_FOLDER="/root/mysql_backup"
 
-echo "Performing MySQL dump for ${DB_NAME}..." [ -d "${BACKUP_FOLDER}" ] || mkdir -p "${BACKUP_FOLDER}" cd "${BACKUP_FOLDER}"
+echo "Performing MySQL dump for ${DB_NAME}..." [ -d "${BACKUP_FOLDER}" ] || mkdir -p "${BACKUP_FOLDER}"
+
+cd "${BACKUP_FOLDER}"
 
 mysqldump --opt -u ${DB_USER} -p${DB_PASSWORD} ${DB_NAME} | gzip > ${DB_NAME}$(date +%F%T).sql.gz
 
